d52f2da8ac23bdf52b5230b88a419171aa04d48c
Vitor de Oliveira Lins <vitorlins485@gmail.com>
2026-01-14 20:43:31 -0300

    feat(util): Add deserialization to JsonUtil

--- src/com/tasktracker/util/JsonUtil.java
+++ src/com/tasktracker/util/JsonUtil.java
@@ -1,6 +1,9 @@
 package com.tasktracker.util;
 
+import com.tasktracker.model.Status;
 import com.tasktracker.model.Task;
+
+import java.time.LocalDateTime;
 
 public class JsonUtil {
     public static String toJson(Task task) {
@@ -18,5 +21,23 @@ public class JsonUtil {
                         task.getStatus(),
                         task.getCreatedAt(),
                         task.getUpdatedAt());
+    }
+
+    public static Task fromJson(String json) {
+        json = json.replace("{", "").replace("}", "");
+        String[] fileds = json.split(",");
+        Task task = new Task();
+        task.setId(Long.parseLong(fileds[0].split(":")[1].strip()));
+        task.setDescription(fileds[1].split(":")[1].replace("\"", "").strip());
+        task.setStatus(Status.valueOf(fileds[2].split(":")[1].replace("\"", "").strip()));
+        task.setCreatedAt(LocalDateTime.parse(fileds[3].split(" ")[2].replace("\"", "").strip()));
+        String updatedAt = fileds[4].split(" ")[2].replace("\"", "").strip();
+        if (updatedAt.contains("null")) {
+            task.setUpdatedAt(null);
+        }
+        else {
+            task.setUpdatedAt(LocalDateTime.parse(updatedAt));
+        }
+        return task;
     }
 }
