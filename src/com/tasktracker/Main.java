c46071dd150206c7057db4ef5fd7037c6588944f
Vitor de Oliveira Lins <vitorlins485@gmail.com>
2026-01-15 15:55:35 -0300

    refactor(main): Configure application initialization

--- src/com/tasktracker/Main.java
+++ src/com/tasktracker/Main.java
@@ -1,10 +1,16 @@
-package com.tasktracker;
+import com.tasktracker.cli.CommandParser;
+import com.tasktracker.exception.ServiceException;
 
-import com.tasktracker.model.Task;
-
-public class Main {
-    static void main() {
-        Task task = new Task(1L, "Test");
-        IO.println(task);
+void main(String[] args) {
+    try {
+        if (args.length == 2) {
+            CommandParser.command(args[0], args[1]);
+        } else if (args.length == 4) {
+            CommandParser.command(args[0], args[1], args[2], args[3]);
+        } else {
+            CommandParser.command(args[0], args[1], args[2]);
+        }
+    } catch (ServiceException | IllegalArgumentException e) {
+        IO.println(e.getMessage());
     }
 }
